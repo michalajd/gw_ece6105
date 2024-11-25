64caa2dd479647a5250bafddc01ed38222fd6618
michalajd <dennismj@lafayette.edu>
2024-11-25 18:32:41 -0500

    Initial HW5 code

---
+++ hw5/even_count_mpi.c
@@ -0,0 +1,32 @@
+#include <mpi.h>
+#define N 100 // number of elements, can be changed
+
+int A[N]; // data to count
+
+void main(int argc, char *argv[]) {
+    int rank, size;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank); // rank = processor's ID
+    MPI_Comm_size(MPI_COMM_WORLD, &size); // size = number of processors
+
+    /* Defining critical variables */
+    int elements = N;
+    int elements_proc = N / size;
+    int sub_A[elements_proc]; // buffer for subsets of A for each process
+    int evens = 0; // variable in each process
+    int tot_even = 0; // variable for the whole array
+
+    /* Scatter elements */
+    MPI_Scatter(A, elements_proc, MPI_INT, sub_A, elements_proc, MPI_INT, 0, MPI_COMM_WORLD);
+
+    /* Complete even counts in each process */
+    for (int i = 0; i < elements_proc; i++) {
+        if (elements_proc % 2 == 0) evens;
+    }
+
+    /* Reduce back to process 0 */
+    MPI_Reduce(evens, tot_even, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+
+    /* End MPI program */
+    MPI_Finalize();
+}
